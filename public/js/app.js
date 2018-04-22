1a60aea0adae237bfb71a2705a6195c9933a1b7e
FuzzyJones <donald.jones83@gmail.com>
2018-04-22 19:02:12 -0400

    trying to get articles json to display dynamically

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,23 @@
-$(function() {
-
-    $("#scrape-button").on("click", function(event) {
-        location.reload();
-    });
-
-    
+function getAllArticles () {
     $.getJSON("/articles", function(data) {
         // For each one
         for (var i = 0; i < data.length; i++) {
             // Display the apropos information on the page
-            $("#display-articles").append("<li class='article-list'>'" + 
+            $("#display-articles").append("<li class='article-list'>" + 
                 "<h3>" + data.title + "</h3>" +
                 "<p>" + data.summary + "</p>" +
                 "<p>" + data.link + "</p></li>");
         }
     });
+};
+
+getAllArticles();
 
 
+$("#scrape-button").on("click", function(event) {
+    location.reload();
 });
+
+    
+ 
+
