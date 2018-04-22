9142519f797303e7188a1d9bddb94574adba810a
FuzzyJones <donald.jones83@gmail.com>
2018-04-22 17:59:33 -0400

    scrape button and route

---
+++ routes/scrape.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/scrape', function(req, res) {
+    res.render("index");
+});
+
+module.exports = router;
