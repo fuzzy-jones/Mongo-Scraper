1bb594afd612ab58fede35c499c7ebe117e880c5
FuzzyJones <donald.jones83@gmail.com>
2018-04-29 16:24:58 -0400

    idk

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             $("#display-articles").append("<li class='article-list'>" + 
                 "<h3>" + data[i].title + "</h3>" +
                 "<p>" + data[i].summary + "</p>" +
-                "<button class='save-article-button' data-articleid=" + data[i]._id + ">Save Article</button>" + 
+                "<button class='save-article-button' data-articleid=" + data[i]._id + " data-articletitle=" + data[i].title + " data-articlesummary=" + data[i].summary + " data-articlesaved=" + data[i].saved + ">Save Article</button>" + 
                 "</li>"
             );
         }
@@ -16,9 +16,25 @@ $(document).ready(function(){
             console.log("i was clicked");
             
             event.preventDefault();
-            var ID = $(this).data("articleid");
-            console.log(ID);
-        });
+            var id = $(this).data("articleid");
+            var saved = $(this).data("articlesaved");
+            console.log(id);
+            console.log(saved);
+
+            // $.ajax("/articles/" + id, {
+            //     type: "PUT",
+            //     data: {
+            //         "saved": true
+            //     }
+            // }).then(
+            //     function() {
+            //     // console.log("changed devour to", newDevour);
+            //     // Reload the page to get the updated list
+            //     location.reload();
+            //     }
+            // );
+
+        })
     });
 
     $.getJSON("/S-articles", function(data) {
